d2bfb6394a1c64f5a6b30f7a1beb6ef9d9d3c98a
gwb121 <80900340@qq.com>
2016-08-03 16:30:52 +0800

    作业提交

---
+++ MusicStore/MusicStore/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+        // GET: Order
+        MusicStoreEntity db = new MusicStoreEntity();
+        public ActionResult Index()
+        {
+            List<Orders> orderList = db.Orders.Where(p => p.Username == User.Identity.Name).ToList();
+            return View(orderList);
+        }
+        public ActionResult Details(int id)
+        {
+            List<OrderDetails> orderDetailList = db.OrderDetails.Where(p => p.OrderId == id).ToList();
+
+            return View(orderDetailList);
+        }
+       
+    }
+}
